fb733d17f94716688756178feb67d7a9bb5d3bba
prachi_singla <prachisingla141@gmail.com>
2024-08-29 11:09:20 +0530

    added data

---
+++ data.js
@@ -0,0 +1,35 @@
+const data = [
+    {
+        name: "Punal",
+        age: 1,
+        section: "b"
+    },
+    {
+        name: "Aman",
+        age: 2,
+        section: "a"
+    },
+    {
+        name: "Riya",
+        age: 3,
+        section: "c"
+    },
+    {
+        name: "Sahil",
+        age: 4,
+        section: "b"
+    },
+ 
+    {
+        name: "sunaina",
+        age: 88,
+        section: "ka"
+    },
+ 
+    {
+        name: "sundari",
+        age: 66,
+        section: "khha"
+    }
+
+];
