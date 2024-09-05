851c2662bbaf19bc9733b38aa6a3e7a1691d9739
prachi_singla <prachisingla141@gmail.com>
2024-09-05 21:42:46 +0530

    hii myself alice

---
+++ average.js
@@ -0,0 +1,7 @@
+db.students.insertMany([
+    { name: "John Doe", age: 22, marks: 90, grade: "A+" },
+    { name: "Jane Smith", age: 18, marks: 85, grade: "A" },
+    { name: "Alice Brown", age: 20, marks: 88, grade: "B" },
+    { name: "Mike Johnson", age: 17, marks: 82, grade: "A+" },
+    { name: "Lucy Davis", age: 22, marks: 75, grade: "B+" }
+])
